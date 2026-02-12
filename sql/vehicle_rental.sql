e733ce496d3cc6149aca9c74d3dbacd83702b09f
volarevin <arizobal.sherwin30@gmail.com>
2026-02-13 03:05:01 +0800

    db upload

--- sql/vehicle_rental.sql
+++ sql/vehicle_rental.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.2.3
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Jan 22, 2026 at 01:42 PM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- Host: localhost:3306
+-- Generation Time: Feb 12, 2026 at 07:04 PM
+-- Server version: 8.4.3
+-- PHP Version: 8.3.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,67 +24,36 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `equipment`
---
-
-CREATE TABLE `equipment` (
-  `equipment_id` int(11) NOT NULL,
-  `name` varchar(100) NOT NULL,
-  `daily_rate` decimal(10,2) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `equipment`
---
-
-INSERT INTO `equipment` (`equipment_id`, `name`, `daily_rate`) VALUES
-(1, 'GPS Navigation', 200.00),
-(2, 'Child Safety Seat', 150.00),
-(3, 'Ski Rack', 300.00),
-(4, 'Dash Cam', 100.00),
-(5, 'GPS Navigation', 200.00),
-(6, 'Child Safety Seat', 150.00),
-(7, 'Ski Rack', 300.00),
-(8, 'Dash Cam', 100.00);
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `reservations`
 --
 
 CREATE TABLE `reservations` (
-  `reservation_id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `vehicle_id` int(11) NOT NULL,
+  `reservation_id` int NOT NULL,
+  `user_id` int NOT NULL,
+  `vehicle_id` int NOT NULL,
   `start_date` date NOT NULL,
   `end_date` date NOT NULL,
-  `status` enum('Pending','Active','Completed','Cancelled') DEFAULT 'Pending',
-  `insurance_added` tinyint(1) DEFAULT 0,
+  `status` enum('Pending','Active','Completed','Cancelled','Rejected') CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT 'Pending',
+  `insurance_added` tinyint(1) DEFAULT '0',
   `total_cost` decimal(10,2) DEFAULT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `cancel_reason` text COLLATE utf8mb4_general_ci,
+  `reject_reason` text COLLATE utf8mb4_general_ci
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `reservations`
 --
 
-INSERT INTO `reservations` (`reservation_id`, `user_id`, `vehicle_id`, `start_date`, `end_date`, `status`, `insurance_added`, `total_cost`, `created_at`) VALUES
-(1, 4, 1, '2026-01-20', '2026-01-22', 'Completed', 0, 3000.00, '2026-01-20 00:54:26'),
-(2, 4, 1, '2026-01-20', '2026-01-22', 'Cancelled', 0, 3000.00, '2026-01-20 00:59:03'),
-(3, 4, 5, '2026-01-22', '2026-01-29', 'Completed', 1, 12250.00, '2026-01-22 02:07:38'),
-(4, 4, 2, '2026-01-22', '2026-01-26', 'Active', 0, 10000.00, '2026-01-22 02:24:50');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `reservation_equipment`
---
-
-CREATE TABLE `reservation_equipment` (
-  `reservation_id` int(11) NOT NULL,
-  `equipment_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+INSERT INTO `reservations` (`reservation_id`, `user_id`, `vehicle_id`, `start_date`, `end_date`, `status`, `insurance_added`, `total_cost`, `created_at`, `cancel_reason`, `reject_reason`) VALUES
+(1, 4, 1, '2026-01-20', '2026-01-22', 'Completed', 0, 3000.00, '2026-01-20 00:54:26', NULL, NULL),
+(2, 4, 1, '2026-01-20', '2026-01-22', 'Cancelled', 0, 3000.00, '2026-01-20 00:59:03', NULL, NULL),
+(3, 4, 5, '2026-01-22', '2026-01-29', 'Completed', 1, 12250.00, '2026-01-22 02:07:38', NULL, NULL),
+(4, 4, 2, '2026-01-22', '2026-01-26', 'Completed', 0, 10000.00, '2026-01-22 02:24:50', NULL, NULL),
+(5, 13, 5, '2026-02-12', '2026-02-17', 'Completed', 1, 5000.00, '2026-02-12 14:07:47', NULL, NULL),
+(6, 4, 1, '2026-02-13', '2026-02-14', 'Cancelled', 0, 1500.00, '2026-02-12 18:01:18', 'maintenance', NULL),
+(7, 13, 2, '2026-02-13', '2026-02-17', 'Cancelled', 1, 12000.00, '2026-02-12 18:28:02', 'change of mind', NULL),
+(8, 13, 2, '2026-02-13', '2026-02-15', 'Rejected', 1, 6000.00, '2026-02-12 18:46:04', NULL, 'maintenance sorry');
 
 -- --------------------------------------------------------
 
@@ -93,13 +62,13 @@ CREATE TABLE `reservation_equipment` (
 --
 
 CREATE TABLE `users` (
-  `user_id` int(11) NOT NULL,
-  `username` varchar(50) NOT NULL,
-  `password_hash` varchar(255) NOT NULL,
-  `first_name` varchar(50) DEFAULT NULL,
-  `last_name` varchar(50) DEFAULT NULL,
-  `role` enum('Admin','Receptionist','Member') NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+  `user_id` int NOT NULL,
+  `username` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  `password_hash` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `first_name` varchar(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `last_name` varchar(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `role` enum('Admin','Receptionist','Member') COLLATE utf8mb4_general_ci NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -110,7 +79,8 @@ INSERT INTO `users` (`user_id`, `username`, `password_hash`, `first_name`, `last
 (1, 'admin', '$2b$12$aJxe.G6esIjlTvME69EXKO8Wub7Mvmk/vUOeMrfbGZbY7B5EpsWHS', 'System', 'Admin', 'Admin', '2026-01-20 00:45:12'),
 (2, 'sherwin', '$2b$12$aJxe.G6esIjlTvME69EXKO8Wub7Mvmk/vUOeMrfbGZbY7B5EpsWHS', 'Sherwin', 'Arizobal', 'Receptionist', '2026-01-20 00:45:12'),
 (3, 'james', '$2b$12$aJxe.G6esIjlTvME69EXKO8Wub7Mvmk/vUOeMrfbGZbY7B5EpsWHS', 'James', 'Banaag', 'Receptionist', '2026-01-20 00:45:12'),
-(4, 'bien', '$2b$12$aJxe.G6esIjlTvME69EXKO8Wub7Mvmk/vUOeMrfbGZbY7B5EpsWHS', 'Bien', 'Hipolito', 'Member', '2026-01-20 00:45:12');
+(4, 'bien', '$2b$12$aJxe.G6esIjlTvME69EXKO8Wub7Mvmk/vUOeMrfbGZbY7B5EpsWHS', 'Bien', 'Hipolito', 'Member', '2026-01-20 00:45:12'),
+(13, 'pogi', '$2b$12$24DL2vamA26WKMSwmTuDbelOreWtElQwUgjq8XSPIGISECe3ATfOy', 'pogi', 'ako', 'Member', '2026-02-12 13:07:31');
 
 -- --------------------------------------------------------
 
@@ -119,16 +89,16 @@ INSERT INTO `users` (`user_id`, `username`, `password_hash`, `first_name`, `last
 --
 
 CREATE TABLE `vehicles` (
-  `vehicle_id` int(11) NOT NULL,
-  `brand` varchar(50) NOT NULL,
-  `model` varchar(50) NOT NULL,
-  `year` int(11) NOT NULL,
-  `license_plate` varchar(20) NOT NULL,
-  `type` enum('Car','Truck','SUV','Van','Motorcycle') NOT NULL,
-  `status` enum('Available','Rented','Maintenance') DEFAULT 'Available',
+  `vehicle_id` int NOT NULL,
+  `brand` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  `model` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  `year` int NOT NULL,
+  `license_plate` varchar(20) COLLATE utf8mb4_general_ci NOT NULL,
+  `type` enum('Car','Truck','SUV','Van','Motorcycle') COLLATE utf8mb4_general_ci NOT NULL,
+  `status` enum('Available','Rented','Maintenance') COLLATE utf8mb4_general_ci DEFAULT 'Available',
   `daily_rate` decimal(10,2) NOT NULL,
-  `image` varchar(255) DEFAULT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+  `image` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -137,7 +107,7 @@ CREATE TABLE `vehicles` (
 
 INSERT INTO `vehicles` (`vehicle_id`, `brand`, `model`, `year`, `license_plate`, `type`, `status`, `daily_rate`, `image`, `created_at`) VALUES
 (1, 'Toyota', 'Vios', 2024, 'ABC 1234', 'Car', 'Available', 1500.00, NULL, '2026-01-20 00:45:12'),
-(2, 'Toyota', 'Innova', 2023, 'DEF 5678', 'Van', 'Rented', 2500.00, NULL, '2026-01-20 00:45:12'),
+(2, 'Toyota', 'Innova', 2023, 'DEF 5678', 'Van', 'Maintenance', 2500.00, NULL, '2026-01-20 00:45:12'),
 (3, 'Toyota', 'Fortuner', 2024, 'GHI 9012', 'SUV', 'Available', 3500.00, NULL, '2026-01-20 00:45:12'),
 (4, 'Mitsubishi', 'L300', 2022, 'JKL 3456', 'Truck', 'Available', 2000.00, NULL, '2026-01-20 00:45:12'),
 (5, 'Honda', 'Click 125i', 2023, 'MNO 7890', 'Motorcycle', 'Available', 500.00, NULL, '2026-01-20 00:45:12'),
@@ -152,12 +122,12 @@ INSERT INTO `vehicles` (`vehicle_id`, `brand`, `model`, `year`, `license_plate`,
 --
 
 CREATE TABLE `vehicle_logs` (
-  `log_id` int(11) NOT NULL,
-  `vehicle_id` int(11) NOT NULL,
-  `user_id` int(11) DEFAULT NULL,
-  `event_type` varchar(50) NOT NULL,
-  `description` text DEFAULT NULL,
-  `log_date` timestamp NOT NULL DEFAULT current_timestamp()
+  `log_id` int NOT NULL,
+  `vehicle_id` int NOT NULL,
+  `user_id` int DEFAULT NULL,
+  `event_type` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  `description` text COLLATE utf8mb4_general_ci,
+  `log_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -166,17 +136,13 @@ CREATE TABLE `vehicle_logs` (
 
 INSERT INTO `vehicle_logs` (`log_id`, `vehicle_id`, `user_id`, `event_type`, `description`, `log_date`) VALUES
 (1, 1, 2, 'Return', 'Standard return', '2026-01-20 00:54:56'),
-(2, 5, 2, 'Return', 'Standard return', '2026-01-22 02:08:21');
+(2, 5, 2, 'Return', 'Standard return', '2026-01-22 02:08:21'),
+(3, 2, 2, 'Return', 'returned just fine', '2026-02-12 17:59:05'),
+(4, 5, 2, 'Return', 'returned with flat tire', '2026-02-12 17:59:22');
 
 --
 -- Indexes for dumped tables
 --
-
---
--- Indexes for table `equipment`
---
-ALTER TABLE `equipment`
-  ADD PRIMARY KEY (`equipment_id`);
 
 --
 -- Indexes for table `reservations`
@@ -185,13 +151,6 @@ ALTER TABLE `reservations`
   ADD PRIMARY KEY (`reservation_id`),
   ADD KEY `user_id` (`user_id`),
   ADD KEY `vehicle_id` (`vehicle_id`);
-
---
--- Indexes for table `reservation_equipment`
---
-ALTER TABLE `reservation_equipment`
-  ADD PRIMARY KEY (`reservation_id`,`equipment_id`),
-  ADD KEY `equipment_id` (`equipment_id`);
 
 --
 -- Indexes for table `users`
@@ -220,34 +179,28 @@ ALTER TABLE `vehicle_logs`
 --
 
 --
--- AUTO_INCREMENT for table `equipment`
---
-ALTER TABLE `equipment`
-  MODIFY `equipment_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
-
---
 -- AUTO_INCREMENT for table `reservations`
 --
 ALTER TABLE `reservations`
-  MODIFY `reservation_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `reservation_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `user_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT for table `vehicles`
 --
 ALTER TABLE `vehicles`
-  MODIFY `vehicle_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `vehicle_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
 --
 -- AUTO_INCREMENT for table `vehicle_logs`
 --
 ALTER TABLE `vehicle_logs`
-  MODIFY `log_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `log_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Constraints for dumped tables
@@ -259,13 +212,6 @@ ALTER TABLE `vehicle_logs`
 ALTER TABLE `reservations`
   ADD CONSTRAINT `reservations_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`),
   ADD CONSTRAINT `reservations_ibfk_2` FOREIGN KEY (`vehicle_id`) REFERENCES `vehicles` (`vehicle_id`);
-
---
--- Constraints for table `reservation_equipment`
---
-ALTER TABLE `reservation_equipment`
-  ADD CONSTRAINT `reservation_equipment_ibfk_1` FOREIGN KEY (`reservation_id`) REFERENCES `reservations` (`reservation_id`),
-  ADD CONSTRAINT `reservation_equipment_ibfk_2` FOREIGN KEY (`equipment_id`) REFERENCES `equipment` (`equipment_id`);
 
 --
 -- Constraints for table `vehicle_logs`
